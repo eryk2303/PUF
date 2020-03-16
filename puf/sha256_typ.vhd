e1b2fe98f764cdaa23b2d9f59b18cfed4047112b
eryk2303 <37594103+eryk2303@users.noreply.github.com>
2020-03-16 14:21:34 +0100

    stałe

---
+++ puf/sha256_typ.vhd
@@ -0,0 +1,17 @@
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+package sha256_typ is
+
+
+		type function_initial_values is array(0 to 7) of std_logic_vector(31 downto 0);
+		type constants_value_sha256 is array(0 to 63) of std_logic_vector(31 downto 0);
+
+
+end sha256_typ;
+
+package body sha256_typ is
+
+end sha256_typ;
